7b2f0f50717f689753185044e3a6634915c17eb3
dominikenzenhofer <dominike@ppedv.de>
2026-01-13 10:38:47 +0100

    Demo Modul 1-2

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Demo
+{
+	public static void Main() 	
+	{
+		/// Das ist ein Kommentar
+		/// sdf
+		/// sdf
+		/// sdf
+
+
+		// boolean können nur mit true oder false zugewiesen werden
+		bool boolean = true;
+		//boolean = 1;
+
+		// Konvertierungen
+		// Zahl zu text
+		int zahl = 42;	
+		string text1 = zahl.ToString(); // Explizit
+		string text2 = $"{zahl}";       // Implizit
+		string text3 = "" + zahl;       // Implizit
+
+		//Text zu Zahl
+		zahl = int.Parse(text1); // ohne Fehlerbehandlung
+		bool funktioniert = int.TryParse(text1, out zahl); // mit Fehlerbehandlung
+		Console.WriteLine($"Funktioniert: {funktioniert}, Zahl: {zahl}");
+		funktioniert = int.TryParse("Hallo", out zahl); // mit Fehlerbehandlung
+		Console.WriteLine($"Funktioniert: {funktioniert}, Zahl: {zahl}");
+
+		// double zu int
+
+		double kommazahl = 3.94;
+		int zahl2 = (int) kommazahl;
+		// Zahl wird abgeschnitten nicht gerundet
+		Console.WriteLine($"Kommazahl: {kommazahl}, Zahl2: {zahl2}");
+
+		Console.WriteLine($"3 / 7 = {(double)zahl2 / 7}");
+
+
+
+
+
+		// Readkey wartet auf eine Eingabe des Benutzers
+		Console.ReadKey();
+	}
+}
+
+
