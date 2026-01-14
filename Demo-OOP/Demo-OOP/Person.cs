02e1e6e5fd057ca026b850e94d5a73096679f2b0
dominikenzenhofer <dominike@ppedv.de>
2026-01-14 09:58:22 +0100

    tag 2 oop - demo

---
+++ Demo-OOP/Demo-OOP/Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_OOP
+{
+	internal class Person
+	{
+		public string Vorname { get; private set; }
+		public string Nachname { get; set; }
+
+		// privates Feld Alter mit Initialwert 0
+		private int alter = 0;
+		// öffentliche Eigenschaft Alter (zum Lesen und Schreiben des Feldes alter)
+		public int Alter 
+		{
+			get
+			{
+				return alter;
+			}
+			set
+			{
+				if(value >= 0)
+				{
+					alter = value;
+				}
+			}
+		}
+
+		private double groesse = 0;
+
+		public double getGroesse() { 
+			return groesse; 
+		}
+		public void setGroesse(double value) 
+		{ 
+			if(value >= 0)
+			{
+				groesse = value; 
+			}
+		}
+
+		// Konstruktor
+		public Person(string vorname, string nachname)
+		{
+			Vorname = vorname;
+			Nachname = nachname;
+		}
+
+		// Konstuktorüberladung mit Alter
+		public Person(string vorname, string nachname, int alter): this(vorname, nachname)
+		{
+			// this.alter = alter; 
+			// Aufruf der Set Methode - mit Validierung
+			Alter = alter; 
+		}
+
+		// Leeren Konstruktor
+		public Person() : this("Max", "Mustermann")
+		{
+		}
+
+		// Methode um Alter anhand eines Geburtsjahres zu setzen
+		public void SetAlterFromGeburtsjahr(int geburtsjahr)
+		{
+			Alter = DateTime.Now.Year - geburtsjahr;
+		}
+
+		// Info-Methode
+		public string Info()
+		{
+			return $"Name: {Vorname} {Nachname}, Alter: {Alter}, Größe: {groesse}cm";
+		}
+	}
+}
---
+++ Demo-OOP/Demo-OOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+
+namespace Demo_OOP {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Person herbert = new Person("Herbert", "Mustermann", 31);
+			Person sabine = new Person("Sabine", "Musterfrau");
+			sabine.SetAlterFromGeburtsjahr(1990);
+			herbert.setGroesse(170);
+			sabine.setGroesse(160);
+
+			Console.WriteLine(sabine.Info());
+			Console.WriteLine(herbert.Info());
+
+		}
+	}
+}
