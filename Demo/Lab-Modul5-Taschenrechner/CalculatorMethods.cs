9b7293c82662ff3d2d35669fca1a710399913103
dominikenzenhofer <dominike@ppedv.de>
2026-01-14 09:55:41 +0100

    tag 2 oop - klassen und namespaces

---
+++ Demo/Lab-Modul5-Taschenrechner/CalculatorMethods.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Program;
+
+namespace Lab_Modul5_Taschenrechner
+{
+	internal static class CalculatorMethods
+	{
+		internal static double Calculate(double number1, double number2, MathematicOperation mo)
+		{
+			//Deklaration und Initialisierung der Ergebnisvariablen
+			double ergebnis = 0.0;
+
+			//Switch zur Auswahl der MathematicOperation
+			switch (mo)
+			{
+				//Berechnung des Ergebnisses
+				case MathematicOperation.Addition:
+					Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
+					return number1 + number2;
+				case MathematicOperation.Subtraktion:
+					Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
+					return number1 - number2;
+				case MathematicOperation.Multiplikation:
+					Console.WriteLine($"{number1} * {number2} = {number1 * number2}");
+					return number1 * number2;
+				case MathematicOperation.Division:
+					//Prüfung einer möglichen Teilung durch null
+					if (number2 == 0)
+					{ 
+						Console.WriteLine("Eine Division durch null ist nicht erlaubt.");
+						return double.NaN;
+					}
+					Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+					return number1 / number2;
+				default:
+					return double.NaN;
+
+			}
+		}
+
+		// Gibt einen Infotext aus und liest eine double Zahl so lange ein bis es funtkioniert.
+		internal static double NumberInput (string text)
+		{
+			bool incorrectInput = true;
+			double number;
+			do
+			{
+				Console.WriteLine(text);
+				incorrectInput = !double.TryParse(Console.ReadLine(), out number);
+				if(incorrectInput)
+					Console.WriteLine("Ungültige Eingabe. Bitte erneut versuchen.");
+
+			} while (incorrectInput);
+			return number;
+		}
+
+		internal static MathematicOperation MathematicOperationInput(string text)
+		{
+			MathematicOperation mo;
+			do
+			{
+				// NumberInput gibt eine double Zahl zurück, diese muss in MathematicOperation gecastet werden.
+				mo = (MathematicOperation) NumberInput(text);
+				
+				// Das wir so lange wiederholt, bis eine gültige MathematicOperation eingegeben wurde.
+			} while ((int)mo < 1 || (int)mo > 4);
+			return mo;
+		}
+	}
+}
---
+++ Demo/Lab-Modul5-Taschenrechner/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using Lab_Modul5_Taschenrechner;
+
+class Program
+{
+	//Enum-Definition
+	internal enum MathematicOperation { Addition = 1, Subtraktion, Multiplikation, Division }
+
+	static void Main(string[] args)
+	{
+		//Schleife für Programm-Wiederholung
+		do
+		{
+			//Eingabe und Parsing der ersten Zahl
+			double zahl1 = CalculatorMethods.NumberInput("\nGib eine Zahl ein: ");
+
+			//Eingabe und Parsing der zweiten Zahl
+			double zahl2 = CalculatorMethods.NumberInput("Gib eine weitere Zahl ein: ");
+			
+
+			//Anzeige der möglichen MathematicOperationen
+			Console.WriteLine("\nWähle eine Rechenoperation:");
+
+			for (int i = 1; i < 5; i++)
+			{
+				Console.WriteLine($"{i}: {(MathematicOperation)i}");
+
+			}
+			//Abfrage der Benutzereingabe
+			
+			MathematicOperation op = CalculatorMethods.MathematicOperationInput("Auswahl: ");
+
+			//Deklaration und Initialisierung der Ergebnisvariablen
+			double ergebnis = CalculatorMethods.Calculate(zahl1, zahl2, op);
+
+			//Frage nach der Wiederholung des Programms
+			Console.WriteLine("Wiederholen? (Y/N) ");
+			//Schleifenbedingungsprüfung anhand Tastendruck des Benutzers
+		} while (Console.ReadKey(true).Key == ConsoleKey.Y);
+	}
+}
+
+
