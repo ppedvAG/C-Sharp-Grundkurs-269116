94c47c0743001639d0a82747fcfaf314b4031fa0
dominikenzenhofer <dominike@ppedv.de>
2026-01-14 15:42:54 +0100

    tag 2 lab modul 9 done

---
+++ Demo-OOP/Lab-Modul9/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Fahrzeugpark
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Schiff schiff = new Schiff("Titanic", 50000, 1000000, Schiff.SchiffsTreibstoff.Dampf);
+			PKW pkw = new PKW("BMW", 250, 50000, 4);
+			Flugzeug flugzeug = new Flugzeug("Boeing 747", 900, 150000000, 416);
+			Container container = new Container("BigContainer");
+
+			Console.WriteLine(BeladeWennBeladbar(schiff, pkw)?.LadungsInfo());
+			Console.WriteLine(BeladeWennBeladbar(schiff, flugzeug)?.LadungsInfo());
+			Console.WriteLine(BeladeWennBeladbar(flugzeug, pkw)?.LadungsInfo());
+			Console.WriteLine(BeladeWennBeladbar(flugzeug, container)?.LadungsInfo());
+			container.Entlade();
+			Console.WriteLine(container.LadungsInfo());
+			Console.WriteLine(BeladeWennBeladbar(schiff, container)?.LadungsInfo());
+		}
+
+		static IBeladbar BeladeWennBeladbar(Object o1, Object o2)
+		{
+			if(o1 is IBeladbar && o2 is Fahrzeug)
+			{
+				((IBeladbar)o1).Belade(o2 as Fahrzeug);
+				return o1 as IBeladbar;
+			} 
+			if(o2 is IBeladbar && o1 is Fahrzeug)
+			{
+				((IBeladbar)o2).Belade(o1 as Fahrzeug);
+				return o2 as IBeladbar;
+			}
+			Console.WriteLine("Keines der Objekte ist beladbar oder Fahrzeug.");
+			return null;
+		}
+	}
+}
