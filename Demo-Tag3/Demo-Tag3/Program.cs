0bd8f4fa5860a8433ce38e91fdef7e01348a15f7
dominikenzenhofer <dominike@ppedv.de>
2026-01-15 12:05:44 +0100

    tag 3 Modul 12

--- Demo-Tag3/Demo-Tag3/Program.cs
+++ Demo-Tag3/Demo-Tag3/Program.cs
@@ -4,49 +4,140 @@
 	{
 		static void Main(string[] args)
 		{
-			// Fehlerbehandlung
-			Console.WriteLine("Zahl eingeben");
+			//// Fehlerbehandlung
+			//Console.WriteLine("Zahl eingeben");
 
-			try
-			{
-				int number = int.Parse(Console.ReadLine());
-			}
-			catch (FormatException e)
-			{
-				Console.WriteLine("Falsches Format, bitte eine Ganze Zahl eingeben");
-			}
-			catch (OverflowException e)
-			{
-				Console.WriteLine("Zahl zu groß");
-			}
-			catch (ArgumentNullException e) // Wird nie Erreicht weil ReadLine nie Null liefert
-			{
-				Console.WriteLine("Parameter von Parse darf nicht Null sein");
-			} finally
-			{
-				Console.WriteLine("Wird immer angezeigt");
-			}
+			//try
+			//{
+			//	int number = int.Parse(Console.ReadLine());
+			//}
+			//catch (FormatException e)
+			//{
+			//	Console.WriteLine("Falsches Format, bitte eine Ganze Zahl eingeben");
+			//}
+			//catch (OverflowException e)
+			//{
+			//	Console.WriteLine("Zahl zu groß");
+			//}
+			//catch (ArgumentNullException e) // Wird nie Erreicht weil ReadLine nie Null liefert
+			//{
+			//	Console.WriteLine("Parameter von Parse darf nicht Null sein");
+			//} finally
+			//{
+			//	Console.WriteLine("Wird immer angezeigt");
+			//}
 
-			// NumberInput mit try catch
-			try
-			{
-				double number = CalculatorMethods.NumberInput("Eine Zahl eingeben:");
-			}
-			catch (IncorrectInputException e)
-			{
-				Console.WriteLine(e.Message);
-			}
+			//// NumberInput mit try catch
+			//try
+			//{
+			//	double number = CalculatorMethods.NumberInput("Eine Zahl eingeben:");
+			//}
+			//catch (IncorrectInputException e)
+			//{
+			//	Console.WriteLine(e.Message);
+			//}
 
 
-			// Calculate mit Division durch 0 soll Exception werfen
-			try
-			{
-				double result = CalculatorMethods.Calculate(12, 0, CalculatorMethods.MathematicOperation.Division);
-			} catch(DivideByZeroException e)
-			{
-				Console.WriteLine(e.Message);
-			}
+			//// Calculate mit Division durch 0 soll Exception werfen
+			//try
+			//{
+			//	double result = CalculatorMethods.Calculate(12, 0, CalculatorMethods.MathematicOperation.Division);
+			//} catch(DivideByZeroException e)
+			//{
+			//	Console.WriteLine(e.Message);
+			//}
 
+			// Listen in C#
+			List<int> numbers = new List<int>();
+			numbers.Add(15);
+			numbers.Add(20);
+			numbers.Add(30);
+			numbers.Add(5);
+			numbers.Add(10);
+			
+
+			// Iteration über Liste mit for-Schleife
+			foreach(int number in numbers)
+			{
+				Console.Write(number+", ");
+			}
+			Console.WriteLine();
+
+			// Listen haben viele nützliche Methoden
+			numbers.Remove(15); // Entfernt 15 aus der Liste
+			numbers.Sort(); // Sortiert die Liste aufsteigend
+			Console.WriteLine("15 entfernt und Liste sortiert:");
+			foreach (int number in numbers)
+			{
+				Console.Write(number + ", ");
+			}
+			Console.WriteLine();
+
+			numbers.RemoveAt(numbers.Count - 1); // Entfernt das letzte Element der Liste
+			Console.WriteLine("Letztes Element entfernt:");
+			foreach (int number in numbers)
+			{
+				Console.Write(number + ", ");
+			}
+			Console.WriteLine();
+
+			numbers.AddRange(numbers); // AddRange kann eine Liste an eine Liste anhängen, in diesem Fall verdoppeln wir die Number Liste
+			Console.WriteLine("Liste Verdoppelt");
+			foreach (int number in numbers)
+			{
+				Console.Write(number + ", ");
+			}
+			Console.WriteLine();
+
+			//numbers.Clear(); // Entfernt alle Elemente aus der Liste
+
+			Console.WriteLine("AVG: "+ numbers.Average()); // Berechnet den Durchschnitt der Zahlen in der Liste
+		
+			
+			// Dictionary
+
+			Dictionary<string, int> AgeOfPersons = new Dictionary<string, int>();
+			AgeOfPersons.Add("Max", 25);
+			AgeOfPersons.Add("Anna", 30);
+			AgeOfPersons.Add("John", 22);
+			//AgeOfPersons.Add("Max", 28); // Führt zu Fehler, da Key "Max" schon existiert
+
+			Console.WriteLine("Alter von Anna: " + AgeOfPersons["Anna"]); // Zugriff auf Wert über Key
+
+
+			// Generische Liste von Klasse genericList verwenden
+			genericList<string> stringList = new genericList<string>();
+			stringList.AddItem("Hallo");
+			genericList<int> intList = new genericList<int>();
+
+			genericList<double> doubleList = new genericList<double>();
+		}
+	}
+
+	// Generische Liste in Klasse
+	class genericList<T>
+	{
+		private List<T> items = new List<T>();
+
+		public T Value1 { get; set; }
+		public T Value2 { get; set; }
+
+
+		public void AddItem(T item)
+		{
+			items.Add(item);
+		}
+		public void RemoveItem(T item)
+		{
+			items.Remove(item);
+		}
+		public void PrintItems()
+		{
+			foreach (T item in items)
+			{
+				Console.Write(item + ", ");
+			}
+			Console.WriteLine();
 		}
 	}
 }
---
+++ Demo-Tag3/Lab-Modul12/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Fahrzeugpark
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Deklaration der benötigten Variablen und und Initialisierung mit Instanzen der benötigten Objekte
+			Queue<Fahrzeug> fzQueue = new Queue<Fahrzeug>();
+			Stack<Fahrzeug> fzStack = new Stack<Fahrzeug>();
+			Dictionary<Fahrzeug, Fahrzeug> fzDict = new Dictionary<Fahrzeug, Fahrzeug>();
+			Random random = new Random();
+			//Deklaration und Initialisierung einer Variablen zur Bestimmung der Anzahl der Durchläufe 
+			int anzahlFahrzeuge = 10;
+
+			//Schleife zur zufälligen Befüllung von Queue und Stack
+			for (int i = 0; i < anzahlFahrzeuge; i++)
+			{
+				Fahrzeug generiertesQF = Fahrzeug.GeneriereFahrzeug($"Q{i}");
+				Fahrzeug generiertesSF = Fahrzeug.GeneriereFahrzeug($"S{i}");
+
+				Console.WriteLine(generiertesSF.ToString());
+				Console.WriteLine(generiertesQF.ToString());
+
+				fzQueue.Enqueue(generiertesQF);
+				fzStack.Push(generiertesSF);
+			}
+
+
+			for (int i = 0; i < anzahlFahrzeuge; i++)
+			{
+				// Methode ohne verwendung der Methode BeladeWennBeladbar (es wird nur auf fzQueue geladen)
+				////Prüfung, ob das Interface vorhanden ist (mittels Peek(), da die Objekte noch benötigt werden)...
+				//if (fzQueue.Peek() is IBeladbar)
+				//{
+				//	//...wenn ja, dann Cast in das Interface und Ausführung der Belade()-Methode (mittels Peek())...
+				//	((IBeladbar)fzQueue.Peek()).Belade(fzStack.Peek());
+				//	//...sowie Hinzufügen zum Dictionary (mittels Pop()/Dequeue(), um beim nächsten Durchlauf andere Objekte an den Spitzen zu haben)
+				//	fzDict.Add(fzQueue.Dequeue(), fzStack.Pop());
+				//}
+				//else
+				//{
+				//	//... wenn nein, dann Löschung der obersten Objekte (mittels Pop()/Dequeue())
+				//	fzQueue.Dequeue();
+				//	fzStack.Pop();
+				//}
+
+				// Methode mit Verwendung der Methode BeladeWennBeladbar (für beide Collections)
+				IBeladbar beladenesObjekt = BeladeWennBeladbar(fzQueue.Peek(), fzStack.Peek());
+				if(beladenesObjekt != null)
+				{
+					fzDict.Add(beladenesObjekt as Fahrzeug, beladenesObjekt.GeladenesFahrzeug);
+					fzQueue.Dequeue();
+					fzStack.Pop();
+				}
+				else
+				{
+					fzQueue.Dequeue();
+					fzStack.Pop();
+				}
+			}
+
+
+			Console.WriteLine("\n----------LADELISTE----------");
+
+			//Schleife zur Ausgabe des Dictionaries
+			foreach (var item in fzDict)
+			{
+				Console.WriteLine($"'{item.Key.Name}' hat '{item.Value.Name}' geladen.");
+			}
+
+		}
+
+		static IBeladbar BeladeWennBeladbar(Object o1, Object o2)
+		{
+			if(o1 is IBeladbar && o2 is Fahrzeug)
+			{
+				((IBeladbar)o1).Belade(o2 as Fahrzeug);
+				return o1 as IBeladbar;
+			} 
+			if(o2 is IBeladbar && o1 is Fahrzeug)
+			{
+				(o2 as IBeladbar).Belade((Fahrzeug)o1);
+				return (IBeladbar)o2;
+			}
+			return null;
+		}
+	}
+}
